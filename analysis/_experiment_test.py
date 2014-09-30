5842849ffd702cf0d7e3baffb3cd8aae0c35d4fa
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-09-30 23:05:46 +0200

    experiment test made independent

--- analysis/_experiment_test.py
+++ analysis/_experiment_test.py
@@ -1,15 +1,8 @@
 import unittest
 
 import numpy
-from sklearn.preprocessing.data import MinMaxScaler
-from sklearn.tree.tree import DecisionTreeClassifier
 
-from analysis.dataset_utils import ArffLoader
 from analysis.experiment import Experiment
-from core.centroid_picker import RandomCentroidPicker
-from core.ensemble_trainer import EnsembleTrainer
-from core.eole import EOLE
-from core.exponential_weigher import ExponentialWeigher
 
 
 __author__ = 'Emanuele Tamponi'
@@ -18,27 +11,43 @@ __author__ = 'Emanuele Tamponi'
 class ExperimentTest(unittest.TestCase):
 
     def setUp(self):
-        self.ensemble = EOLE(
-            n_experts=2,
-            ensemble_trainer=EnsembleTrainer(
-                DecisionTreeClassifier(max_depth=1),
-                centroid_picker=RandomCentroidPicker(),
-                weigher_sampler=ExponentialWeigher(precision=1, power=2)
-            ),
-            preprocessor=MinMaxScaler(),
-            use_probs=True,
-            use_competences=False
-        )
         self.experiment = Experiment(
-            "test_experiment", self.ensemble, ArffLoader("tests/dataset.arff"), folds=2, repetitions=10
+            name="test_experiment",
+            ensemble=FakeEOLE(n_experts=5),
+            dataset_loader=FakeLoader(n=100, n_features=5, n_labels=3),
+            folds=2, repetitions=10
         )
 
     def test_run_returns_report(self):
         report = self.experiment.run()
-
         self.assertEqual(20, report.sample_size)
 
     def test_each_repetition_is_different(self):
         report = self.experiment.run()
-
         self.assertFalse(numpy.array_equal(report.accuracy_sample[0], report.accuracy_sample[10]))
+
+
+class FakeEOLE(object):
+
+    def __init__(self, n_experts):
+        self.n_experts = n_experts
+        self.labels = None
+
+    def train(self, instances, labels):
+        self.labels = numpy.unique(labels)
+
+    def predict(self, instances):
+        return numpy.asarray(
+            [numpy.random.choice(self.labels, size=len(instances)) for _ in range(self.n_experts)]
+        ).transpose()
+
+
+class FakeLoader(object):
+
+    def __init__(self, n, n_features, n_labels):
+        self.n = n
+        self.n_features = n_features
+        self.n_labels = n_labels
+
+    def load_dataset(self):
+        return numpy.random.randn(self.n, self.n_features), numpy.random.choice(self.n_labels, size=self.n)
