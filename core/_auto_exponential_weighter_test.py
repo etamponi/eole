98328fd0153832aa7706a6e0775592f54c96a9bf
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2014-09-30 17:40:29 +0200

    Fixed a bug in auto exponential weigher: was using variances instead of precisions...

--- core/_auto_exponential_weighter_test.py
+++ core/_auto_exponential_weighter_test.py
@@ -16,10 +16,25 @@ class AutoExponentialWeigherTest(unittest.TestCase):
             [-0.5, -1.0],
             [+1.3, +0.5]
         ])
-        expected_variances = numpy.diag(numpy.asarray([0.56, 1.50]))
+        expected_precision_matrix = numpy.diag(numpy.asarray([0.56, 1.50])**-1)
         weigher = AutoExponentialWeigher(scale=1)
         weigher.train(instances)
-        numpy.testing.assert_array_almost_equal(expected_variances, weigher.variance_matrix)
+        numpy.testing.assert_array_almost_equal(expected_precision_matrix, weigher.precision_matrix)
 
     def test_weights(self):
-        pass
+        train_instances = numpy.asarray([
+            [+2.0, +2.0],
+            [-1.0, -1.0],
+            [+0.5, +0.5]
+        ])
+        weigher = AutoExponentialWeigher(scale=2)
+        weigher.train(train_instances)
+        test_instances = numpy.asarray([
+            [0.0, 1.0],
+            [1.0, -1.],
+            [1.0, 2.0]
+        ])
+        centroid = numpy.zeros(2)
+        factor = -1.0 / 1.5  # -0.5 * scale / variance
+        expected_weights = numpy.exp([factor * 1.0, factor * 2.0, factor * 5.0])
+        numpy.testing.assert_array_almost_equal(expected_weights, weigher.get_weights(test_instances, centroid))
